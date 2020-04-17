55c5c977900b20a724485d80c053dbb1b172ab60
kboggle <ketki88@gmail.com>
2020-04-17 19:09:17 +0000

    Add files via upload

---
+++ robot_P1/script/hello.cpp
@@ -0,0 +1,20 @@
+#include <gazebo/gazebo.hh>
+
+namespace gazebo
+
+{
+ class WorldPluginMyRobot : public WorldPlugin
+ {
+  public: WorldPluginMyRobot() : WorldPlugin()
+  {
+   printf("Hello welcome to KG's world\n");
+  }
+
+  public: void Load(physics::WorldPtr _world, sdf::ElementPtr _sdf)
+
+          {
+          }
+ };
+ GZ_REGISTER_WORLD_PLUGIN(WorldPluginMyRobot)
+}
+
